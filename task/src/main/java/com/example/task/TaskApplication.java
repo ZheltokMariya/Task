ef0398df51c7a5942caee919dff6b9e11dcec5b4
Zheltok Mariya <masha.zheltok@gmail.com>
2020-02-10 05:09:25 +0300

    correct mistakes

--- task/src/main/java/com/example/task/TaskApplication.java
+++ task/src/main/java/com/example/task/TaskApplication.java
@@ -7,14 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 @SpringBootApplication
 public class TaskApplication {
-
-	private static final Logger log = LoggerFactory.getLogger(TaskApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
